67e22b70fef5147030987f4fa2be43cde76d57ff
ccc-kazuyuki-ono <kazuyuki.ono@ccc.co.jp>
2023-06-23 15:57:24 +0900

    update 2

--- docker-images/app/main.py
+++ docker-images/app/main.py
@@ -9,5 +9,5 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    message = f"おのですよ！ 2023/06/23 Hello world! From FastAPI running on Uvicorn with Gunicorn. Using Python {version}"
+    message = f"おのですよ！ 2023/06/23-2 Hello world! From FastAPI running on Uvicorn with Gunicorn. Using Python {version}"
     return {"message": message}
